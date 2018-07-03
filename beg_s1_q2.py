04c54caaddeaf7b342eb910163e63c29608e2514
manunag <36444982+manunag@users.noreply.github.com>
2018-07-03 19:05:20 +0530

    update

--- beg_s1_q2.py
+++ beg_s1_q2.py
@@ -1,5 +1,8 @@
 N=int(input())
-if N%2==0:
-    print("Even")
-else
-    print("Odd")
+if 1<=N<=100000:
+    if N%2==0:
+        print("Even")
+    else
+        print("Odd")
+else:
+    print("Invalid Number")
