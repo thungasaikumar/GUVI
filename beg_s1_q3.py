143951931ef0946cf349379a1f28be3b232ddbe4
manunag <36444982+manunag@users.noreply.github.com>
2018-07-03 19:43:30 +0530

    Update beg_s1_q3.py

--- beg_s1_q3.py
+++ beg_s1_q3.py
@@ -1,7 +1,7 @@
 x=str(input())
 if x.isalpha():
     if x=="a" or x=="e" or x=="i" or x=="o" or x=="u" or x=="A" or x=="E" or x=="I" or x=="O" or x=="U":
-        print("Ovel")
+        print("Vowel")
     else:
         print("Consonant")
 else:
