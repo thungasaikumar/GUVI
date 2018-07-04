9094a7e7389ddb18daf5b4a9bf3c5b7e94e7d0c9
manunag <36444982+manunag@users.noreply.github.com>
2018-07-04 17:53:23 +0530

    Update beg_s1_q5.py

--- beg_s1_q5.py
+++ beg_s1_q5.py
@@ -3,12 +3,13 @@ b=input()
 c=input()
 if a.isalpha() or b.isalpha() or c.isalpha():
     print("Invalid")
-a=int(a)
-b=int(b)
-c=int(c)
-if a>=b and a>=c:
-    print(a)
-elif b>=a and b>=c:
-    print(b)
-elif c>=a and c>=b:
-    print(c)
+else:
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    if a>=b and a>=c:
+        print(a)
+    elif b>=a and b>=c:
+        print(b)
+    elif c>=a and c>=b:
+        print(c)
