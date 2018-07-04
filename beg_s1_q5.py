ddcc9460ad902e3c1733d2d126af3cea43244391
manunag <36444982+manunag@users.noreply.github.com>
2018-07-04 17:39:43 +0530

    Create beg_s1_q5.py

---
+++ beg_s1_q5.py
@@ -0,0 +1,14 @@
+a=input()
+b=input()
+c=input()
+if a.isalpha() or b.isalpha() or c.isalpha():
+    print("invalid")
+a=int(a)
+b=int(b)
+c=int(c)
+if a>=b and a>=c:
+    print(a)
+elif b>=a and b>=c:
+    print(b)
+elif c>=a and c>=b:
+    print(c)
