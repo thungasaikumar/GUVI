d42552b1452b7cb074e1848ba66ec4ad1aef48dc
manunag <36444982+manunag@users.noreply.github.com>
2018-07-05 19:58:00 +0530

    Update beg13.py

--- beg13.py
+++ beg13.py
@@ -3,13 +3,13 @@ count=0
 if x.isalpha():
     print("Invaild")
 elif int(x)==1:
-    print("No")
+    print("no")
 else:
     x=int(x)
     for i in range(2,x):
         if x%i==0:
             count=1
     if count==1:
-        print("No")
+        print("no")
     else:
-        print("Yes")
+        print("yes")
