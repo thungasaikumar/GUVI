0db13b6d5cf75d0b649e116f58d874cc20ff3c86
manunag <36444982+manunag@users.noreply.github.com>
2018-07-05 20:45:22 +0530

    Create beg15.py

---
+++ beg15.py
@@ -0,0 +1,10 @@
+x=input()
+y=input()
+if x.isalpha() or y.isalpha():
+    print("Invaild")
+else:
+    x=int(x)
+    y=int(y)
+    for i in range((x+1),y):
+        if i%2==0:
+            print(i,end=' ')
