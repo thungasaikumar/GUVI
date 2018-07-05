195269c3bea76c57000ce6a7650546e43767c275
manunag <36444982+manunag@users.noreply.github.com>
2018-07-05 19:54:47 +0530

    Create beg13.py

---
+++ beg13.py
@@ -0,0 +1,15 @@
+x=input()
+count=0
+if x.isalpha():
+    print("Invaild")
+elif int(x)==1:
+    print("No")
+else:
+    x=int(x)
+    for i in range(2,x):
+        if x%i==0:
+            count=1
+    if count==1:
+        print("No")
+    else:
+        print("Yes")
