0b822254521a20d99edd3f9997783bc7fe994beb
manunag <36444982+manunag@users.noreply.github.com>
2018-07-08 10:06:07 +0530

    Create beg17.py

---
+++ beg17.py
@@ -0,0 +1,16 @@
+x=input()
+if x.isalpha():
+    print("Invaild")
+else:
+    x=int(x)
+    y=int(x)
+    arm=int(0)
+    while y!=0:
+        z=y%10;
+        arm=arm+(z**3)
+        y=int(y/10)
+    print(arm)
+    if arm==x:
+        print("yes")
+    else:
+        print("no")
